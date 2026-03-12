98445a9f8ee0a85196023e4cbaa2ef2eeaf24462
Brenno Marques <brennoms.estudante@gmail.com>
2026-03-12 16:32:36 -0300

    Refactor Content initialization to use updated properties for style and size

--- main.py
+++ main.py
@@ -4,37 +4,38 @@ from terminui.terminui import Terminui
 from terminui.core.content import Content
 
 terminui = Terminui()
+terminui.style.bg.color = 'cyan'
 
 content = Content()
-content.background_color = 'green'
-content.width = 10
-content.height = 5
-content.pos_x = 10
-content.pos_y = 5
+content.style.bg.color = 'green'
+content.size.width = 10
+content.size.height = 5
+content.pos.x = 10
+content.pos.y = 5
 terminui.addContent(content)
 
 content2 = Content()
-content2.background_color = 'red'
-content2.width = 10
-content2.height = 5
-content2.pos_x = 20
-content2.pos_y = 10
+content2.style.bg.color = 'red'
+content2.size.width = 10
+content2.size.height = 5
+content2.pos.x = 20
+content2.pos.y = 10
 terminui.addContent(content2)
 
 content3 = Content()
-content3.background_color = 'blue'
-content3.width = 10
-content3.height = 5
-content3.pos_x = 20
-content3.pos_y = 5
+content3.style.bg.color = 'blue'
+content3.size.width = 10
+content3.size.height = 5
+content3.pos.x = 20
+content3.pos.y = 5
 terminui.addContent(content3)
 
 content4 = Content()
-content4.background_color = 'blue'
-content4.width = 10
-content4.height = 5
-content4.pos_x = 10
-content4.pos_y = 10
+content4.style.bg.color = 'white'
+content4.size.width = 10
+content4.size.height = 5
+content4.pos.x = 10
+content4.pos.y = 10
 terminui.addContent(content4)
 
 terminui.start()
